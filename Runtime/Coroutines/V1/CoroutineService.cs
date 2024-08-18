2693dc33f98559929ae549e5a90022ff85cbe4ac
ETdoFresh <ETdoFresh@gmail.com>
2024-08-17 23:43:32 -0500

    Add [Preserve] attribute for AOT compilation in CoroutineService, EventService, and UnityEventService
    To enhance compatibility with AOT (Ahead-of-Time) compilation, the [Preserve] attribute is added to the CoroutineService, EventService, and UnityEventService classes. This attribute ensures that classes and methods are not removed from the compiled code during the AOT compilation process, safeguarding their functionality across various platforms and devices.
    
    Changes in CoroutineService.cs:
    - Added "using UnityEngine.Scripting" to import the necessary namespace for the [Preserve] attribute.
    - Applied the [Preserve] attribute to the CoroutineService class.
    
    Changes in EventService.cs:
    - Added "using UnityEngine.Scripting" to import the necessary namespace for the [Preserve] attribute.
    - Applied the [Preserve] attribute to the EventService class.
    
    Changes

--- Runtime/Coroutines/V1/CoroutineService.cs
+++ Runtime/Coroutines/V1/CoroutineService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace SharedServices.Coroutines.V1
 {
+    [Preserve]
     public class CoroutineService : ICoroutineService, IDisposable
     {
         private static CoroutineHelper _coroutineHelper = new CoroutineEditorHelper();
